bac8bfe429d2a1c35621385e646f525123ae4730
Eduardo honório de Vasconcelos <138134896+eduhv01@users.noreply.github.com>
2025-06-03 20:42:58 -0300

    Subindo Tabela Palestrante

--- G_Eventos.sql
+++ G_Eventos.sql
@@ -9,6 +9,13 @@ CREATE TABLE Evento (
     local_evento VARCHAR(100)
 );
 
+CREATE TABLE Palestrante (
+    cod_palestrante INT PRIMARY KEY AUTO_INCREMENT,
+    nomeCompleto VARCHAR(155) NOT NULL,
+    areaDeAtuacao VARCHAR(80) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
+
 CREATE TABLE Participante(
 	cod_participante INT auto_increment PRIMARY KEY NOT NULL,
     nome VARCHAR(100) NOT NULL,
